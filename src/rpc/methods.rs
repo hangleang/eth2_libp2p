f4321f22997f2f6d5446229654019e051ea2565b
Tumas <tumas@grandine.io>
2024-05-17 18:05:53 +0300

    Wrap columns request param into Arc

--- src/rpc/methods.rs
+++ src/rpc/methods.rs
@@ -300,7 +300,7 @@ pub struct DataColumnsByRangeRequest {
     /// The number of slots from the start slot.
     pub count: u64,
     /// The list column indices being requested.
-    pub columns: ContiguousList<ColumnIndex, NumberOfColumns>,
+    pub columns: Arc<ContiguousList<ColumnIndex, NumberOfColumns>>,
 }
 
 impl DataColumnsByRangeRequest {
