527ab4ee8cb39e757431f2003df8a5faecb0b516
guha-rahul <69rahul16@gmail.com>
2024-07-13 02:37:55 +0530

    backport_22fe0a6

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1432,19 +1432,17 @@ impl<AppReqId: ReqId, P: Preset> Network<AppReqId, P> {
     ) -> Option<NetworkEvent<AppReqId, P>> {
         let peer_id = event.peer_id;
 
-        if !self.peer_manager().is_connected(&peer_id) {
-            // Sync expects a RPCError::Disconnected to drop associated lookups with this peer.
-            // Silencing this event breaks the API contract with RPC where every request ends with
-            // - A stream termination event, or
-            // - An RPCError event
-            if !matches!(event.event, HandlerEvent::Err(HandlerErr::Outbound { .. })) {
-                debug!(
-                    self.log,
-                    "Ignoring rpc message of disconnecting peer";
-                    event
-                );
-                return None;
-            }
+        // Do not permit Inbound events from peers that are being disconnected, or RPC requests.
+        if !self.peer_manager().is_connected(&peer_id)
+            && (matches!(event.event, HandlerEvent::Err(HandlerErr::Inbound { .. }))
+                || matches!(event.event, HandlerEvent::Ok(RPCReceived::Request(..))))
+        {
+            debug!(
+                self.log,
+                "Ignoring rpc message of disconnecting peer";
+                event
+            );
+            return None;
         }
 
         let handler_id = event.conn_id;
